57f4bb6aa7527bf20262d9aa126f714fa7553a3c
zeegomo <g.pasini98@gmail.com>
2018-10-25 14:08:08 +0200

    key appended to counter instead of xored

--- src/jacopone.rs
+++ src/jacopone.rs
@@ -11,24 +11,12 @@ pub fn jacopone_encrypt_ctr(message: &[u8], key: &[u8], nonce: &[u8], counter: u
         ciphertext.extend_from_slice(&xor(&block_encrypt(&block_counter, key), &message[64 * i.. 64 * i + 64]));
     }
     let block_counter = get_block_counter(nonce, & mut c);
-    ciphertext.extend_from_slice(&xor(&message[message.len()/64..], &block_encrypt(&block_counter, key)));
+    ciphertext.extend_from_slice(&xor(&message[(message.len()/64) * 64..], &block_encrypt(&block_counter, key)));
     ciphertext
 }
 
-pub fn jacopone_decrypt_ctr(message: &[u8], key: &[u8], nonce: &[u8], counter: u64) -> Vec<u8> {
-    let mut c = counter;
-    let mut plaintext = Vec::new();
-    for i in 0..message.len()/64 {
-        let block_counter = get_block_counter(nonce, & mut c);
-        plaintext.extend_from_slice(&xor(&block_decrypt(&block_counter, key), &message[64 * i.. 64 * i + 64]));
-    }
-    let block_counter = get_block_counter(nonce, & mut c);
-    plaintext.extend_from_slice(&xor(&message[message.len()/64..], &block_encrypt(&block_counter, key)));
-    plaintext   
-}
-
 fn block_encrypt(message: &[u8], key: &[u8]) -> Vec<u8> {
-    let mut ciphertext = pad(message, 64);
+    let mut ciphertext = message.clone().to_vec();
     for _i in 0..4 {
         ciphertext = feistel_round(&ciphertext, key);
         ciphertext = swap(&ciphertext);
@@ -36,7 +24,7 @@ fn block_encrypt(message: &[u8], key: &[u8]) -> Vec<u8> {
     ciphertext
 } 
 
-fn block_decrypt(message: &[u8], key: &[u8]) -> Vec<u8> {
+/*fn block_decrypt(message: &[u8], key: &[u8]) -> Vec<u8> {
     let mut plaintext = message.clone().to_vec();
     for _i in 0..4 {
         plaintext = swap(&plaintext);
@@ -44,7 +32,7 @@ fn block_decrypt(message: &[u8], key: &[u8]) -> Vec<u8> {
     }
     plaintext
 }
-
+*/ 
 fn feistel_round(block: &[u8], key: &[u8]) -> Vec<u8> {
     let l = &block[0..32];
     let r = &block[32..];
@@ -60,7 +48,7 @@ fn swap(block: &[u8]) -> Vec<u8> {
     r
 }
 
-fn xor(s1: &[u8], s2: &[u8]) -> Vec<u8> {
+pub fn xor(s1: &[u8], s2: &[u8]) -> Vec<u8> {
     s1.iter().zip(s2).map(|(x, y)| x ^ y).collect() 
 }
 
@@ -82,7 +70,9 @@ fn pad(text: &[u8], length: u8) -> Vec<u8> {
 
 fn hash(block: &[u8], key: &[u8]) -> Vec<u8> {
     let mut hasher = Sha3::sha3_256();
-    hasher.input(&xor(block, key));
+    let mut round_block = block.to_vec();
+    round_block.extend_from_slice(key);
+    hasher.input(&round_block);
     hex_to_bytes(&hasher.result_str())
 }
 
@@ -100,17 +90,16 @@ fn hex_to_bytes(string: &str) -> Vec<u8> {
 fn get_block_counter(nonce: &[u8], counter: & mut u64) -> Vec<u8> {
     let mut n = nonce.clone().to_vec();
     n.extend_from_slice(&(to_bytes(*counter)));
-    counter.wrapping_add(1);
+    *counter = (*counter).wrapping_add(1);
     n  
 }
 
 fn to_bytes(n: u64) -> Vec<u8> {
     let mut n = n;
     let mut v = Vec::new();
-    for i in 0..4{
-        v.push((n & 256) as u8);
+    for _i in 0..4{
+        v.push((n & 255) as u8);
         n = n >> 8;
     }
-    println!("{:?}", v);
     v
 }
