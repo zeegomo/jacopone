fda5d7585c70f2e63e623809f28fc31a8d1f417a
zeegomo <g.pasini98@gmail.com>
2018-10-25 10:54:00 +0200

    test main for ctr

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ extern crate crypto;
 
 mod jacopone;
 
+
+
+
 use self::crypto::digest::Digest;
 use self::crypto::sha3::Sha3;
 use jacopone::*;
 
 fn main() {
-	let message = "ciao".as_bytes();
+	let message = "1000000001001001010010010010101101010101".as_bytes();
+	let mut nonce = Vec::new();
+	for i in 0..56 {
+		nonce.push(22);
+	}
 	let key = [10, 7, 21, 33, 32, 76, 54, 45, 12, 87, 09, 12, 43, 87, 43 ,23, 44, 21, 33, 32, 76, 54, 45, 12, 87, 09, 12, 43, 87, 43 ,23, 44];
-	let ciphertext = jacopone_encrypt(&message, &key);
+	let ciphertext = jacopone_encrypt_ctr(&message, &key, &nonce, 0);
 	println!("ciphertext: {:?}", String::from_utf8_lossy(&ciphertext));
-	let decrypted = jacopone_decrypt(&ciphertext, &key);
-	println!("{}", String::from_utf8_lossy(&decrypted));
+	let decrypted = jacopone_decrypt_ctr(&ciphertext, &key, &nonce, 0);
+	println!("decrypted: {:?}", String::from_utf8_lossy(&decrypted));
+    let m: u64 = 34;
+    
 }
 
