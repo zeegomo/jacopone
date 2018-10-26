e784e312ade771ded69d1ff82e9d0e2b41ebd290
zeegomo <g.pasini98@gmail.com>
2018-10-26 14:04:13 +0200

    refactor

--- src/jacopone/mod.rs
+++ src/jacopone/mod.rs
@@ -1,5 +1,5 @@
-use crypto::digest::Digest;
-use crypto::sha3::Sha3;
+mod utils;
+use self::utils::*;
 use std::sync::mpsc;
 
 pub fn jacopone_encrypt_ctr_threaded(message: &[u8], key: &[u8], nonce: &[u8], counter: u64, thread_count: u8) -> Vec<u8> {
@@ -35,9 +35,9 @@ pub fn jacopone_encrypt_ctr_threaded(message: &[u8], key: &[u8], nonce: &[u8], c
                     let c = counter + start as u64;
                     let ciphertext = jacopone_encrypt_ctr(&message[start * 64 .. end * 64], key, nonce, c);
                     tx.send(ciphertext).unwrap();
-                    });
-                }      
-            }
+                });
+            }      
+        }
     });
 
     //receive results from threads
@@ -68,10 +68,10 @@ pub fn get_thread_blocks(message_len: usize, thread_count: u8) -> Vec<[u64; 2]>{
     let mut blocks_index = Vec::new(); 
     let block_num = message_len / 64;
     //if block_num / thread_count  as u64 > 0 {
-        for _i in 0..thread_count {
-            partition.push(block_num / thread_count as u64);
-        }
-    
+    for _i in 0..thread_count {
+        partition.push(block_num / thread_count as u64);
+    }
+
     let mut res = block_num - (block_num / thread_count as u64) * thread_count as u64;
     for i in 0..thread_count as usize {
         if res > 0 {
@@ -79,7 +79,7 @@ pub fn get_thread_blocks(message_len: usize, thread_count: u8) -> Vec<[u64; 2]>{
             res = res - 1;
         }
     }
-    
+
     blocks_index.push([0, partition[0]]);
     let mut last = partition[0];
     for i in 1..thread_count as usize {
@@ -129,49 +129,3 @@ fn feistel_round(block: &[u8], key: &[u8]) -> Vec<u8> {
     l
 }
 
-fn swap(block: &[u8]) -> Vec<u8> {
-    let l = &block[0..32];
-    let mut r = (&block[32..]).to_vec();
-    r.extend_from_slice(l);
-    r
-}
-
-pub fn xor(s1: &[u8], s2: &[u8]) -> Vec<u8> {
-    s1.iter().zip(s2).map(|(x, y)| x ^ y).collect() 
-}
-
-fn hash(block: &[u8], key: &[u8]) -> Vec<u8> {
-    let mut hasher = Sha3::sha3_256();
-    let mut round_block = block.to_vec();
-    round_block.extend_from_slice(key);
-    hasher.input(&round_block);
-    hex_to_bytes(&hasher.result_str())
-}
-
-fn hex_to_bytes(string: &str) -> Vec<u8> {
-    let mut bytes = Vec::new();
-    for i in 0..(string.len() / 2) {
-        match u8::from_str_radix(&string[2 * i.. 2 * i + 2], 16) {
-            Ok(n) => bytes.push(n),
-            Err(_) => panic!("Error in hex conversion"),
-        }
-    }
-    bytes
-}
-
-fn get_block_counter(nonce: &[u8], counter: & mut u64) -> Vec<u8> {
-    let mut n = nonce.clone().to_vec();
-    n.extend_from_slice(&(to_bytes(*counter)));
-    *counter = (*counter).wrapping_add(1);
-    n  
-}
-
-fn to_bytes(n: u64) -> Vec<u8> {
-    let mut n = n;
-    let mut v = Vec::new();
-    for _i in 0..4{
-        v.push((n & 255) as u8);
-        n = n >> 8;
-    }
-    v
-}
