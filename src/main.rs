ccebbac674ea370601425fe1d0b0f6157c46be27
Giacomo Pasini <g.pasini98@gmail.com>
2020-04-08 23:15:47 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,51 @@
 extern crate clap;
 extern crate jacopone;
 
-use std::time::{Instant};
-use std::fs::File;
-use std::io::prelude::*;	
-use jacopone::*;
 use clap::App;
+use jacopone::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::time::Instant;
 
 fn main() {
-
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let message = get_text_from_file(matches.value_of("INPUT").unwrap());
-    let key = matches.value_of("KEY").unwrap().as_bytes().to_vec();
+    let mut message = get_text_from_file(matches.value_of("INPUT").unwrap());
+    let mut key = matches.value_of("KEY").unwrap().as_bytes().to_vec();
+    key.resize(32, 0);
     let nonce = matches.value_of("NONCE").unwrap().as_bytes().to_vec();
-    let counter = u64::from_str_radix(matches.value_of("COUNTER").unwrap(), 10).unwrap();
-    let threads = u8::from_str_radix(matches.value_of("threads").unwrap_or("2"), 10).unwrap();
-    let output = matches.value_of("output").unwrap_or(matches.value_of("INPUT").unwrap());
+
+    let output = matches
+        .value_of("output")
+        .unwrap_or(matches.value_of("INPUT").unwrap());
 
     let now = Instant::now();
+    println!("Encrypting {} bytes...", message.len());
+    let jacopone = Jacopone::<ModeCTR, Sha2, Dummy>::new();
+    jacopone.encrypt(&mut message, &key, &nonce);
 
-    let jacopone = Jacopone::new(threads);
-    let data = CipherData::new(message, key, nonce, counter);
-    let ciphertext = jacopone.encrypt(data);
-
-    println!("elapsed: {:?}",now.elapsed().as_secs() as f64
-           + now.elapsed().subsec_nanos() as f64 * 1e-9 );
-
-    write_to_file(output, &ciphertext);
-
+    println!(
+        "Done: {:?}",
+        now.elapsed().as_secs() as f64 + now.elapsed().subsec_nanos() as f64 * 1e-9
+    );
+    write_to_file(output, &message);
 }
 
-fn write_to_file(filename: &str, content: &[u8]){
-	let mut file = match File::create(filename) {
+fn write_to_file(filename: &str, content: &[u8]) {
+    let mut file = match File::create(filename) {
         Ok(file) => file,
         Err(_) => panic!("no such file"),
     };
-    file.write_all(content);
+    file.write_all(content).unwrap();
 }
 
-fn get_text_from_file(filename: &str) -> Vec<u8>{
+fn get_text_from_file(filename: &str) -> Vec<u8> {
     let mut file = match File::open(filename) {
         Ok(file) => file,
         Err(_) => panic!("no such file"),
     };
     let mut text = Vec::new();
-    file.read_to_end(&mut text)
-        .ok()
-        .expect("failed to read!");
+    file.read_to_end(&mut text).ok().expect("failed to read!");
     text
 }
-
-
